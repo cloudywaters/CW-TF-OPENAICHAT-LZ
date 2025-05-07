d1df240556cb3da5c550acbed51823caeb132f10
Mick <62369997+cloudywaters@users.noreply.github.com>
2025-05-07 04:16:25 +0000

    fix providers

--- providers.tf
+++ providers.tf
@@ -1,14 +1,13 @@
 terraform {
-  required_version = ">= 1.11"
-
+  required_version = ">= 1.3"
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">=3.71.0, <5.0.0"
+      version = "~> 3.74.0"
     }
     azapi = {
       source  = "azure/azapi"
-      version = ">=1.13.0, <3.0.0"
+      version = "~> 1.13.0"
     }
   }
 }
